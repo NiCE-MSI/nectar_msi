fcbf803c5c935d329584fcaf8082e3bc6ef6a601
Ariadna Gonzalez <ariadna.gonzalez@npl.co.uk>
2023-09-06 15:53:19 +0100

    rename nectar_msi

--- nectar_msi/__init__.py
+++ nectar_msi/__init__.py
@@ -1,17 +1,17 @@
 """nectar - nectar (NoisE CorrecTion AlgoRithm) is a python package for noise correction in Q-ToF MSI instruments."""
 
-from nectar.data_formats.data_cube import DataCube
-from nectar.data_formats.data_spectrum import DataSpectrum
-from nectar.data_processing.readers import Readers
-from nectar.data_processing.savers import Savers
-from nectar.data_processing.data_operations import DataOperations
-from nectar.noise_correction.noise_determination import NoiseDetermination
-from nectar.noise_correction.noise_correction import NoiseCorrection
-from nectar.noise_correction.chemical_noise import ChemicalNoise
-from nectar.noise_correction.background_noise import BackgroundNoise
-from nectar.database_matching.database_matching import DatabaseMatching
-from nectar.plotting.plotting import Plotting
-from nectar.peak_picking.peak_picking import PeakPicking
+from nectar_msi.data_formats.data_cube import DataCube
+from nectar_msi.data_formats.data_spectrum import DataSpectrum
+from nectar_msi.data_processing.readers import Readers
+from nectar_msi.data_processing.savers import Savers
+from nectar_msi.data_processing.data_operations import DataOperations
+from nectar_msi.noise_correction.noise_determination import NoiseDetermination
+from nectar_msi.noise_correction.noise_correction import NoiseCorrection
+from nectar_msi.noise_correction.chemical_noise import ChemicalNoise
+from nectar_msi.noise_correction.background_noise import BackgroundNoise
+from nectar_msi.database_matching.database_matching import DatabaseMatching
+from nectar_msi.plotting.plotting import Plotting
+from nectar_msi.peak_picking.peak_picking import PeakPicking
 
 __author__ = "Ariadna Gonzalez-Fernandez <ariadna.gonzalez@npl.co.uk>"
 __all__ = []
